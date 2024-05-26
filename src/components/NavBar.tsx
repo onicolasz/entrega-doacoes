238983dfb92f0048246dbd3e4d8899c516606f84
Leonardo Mayca <leomt8440@gmail.com>
2024-05-26 18:14:16 -0300

    feat: add new style from navbar.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,52 +5,70 @@ import { useRouter } from "next/navigation";
 import { useContext } from "react";
 import { usePathname } from 'next/navigation'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome } from "@fortawesome/free-solid-svg-icons";
-import ProfileButton from "./ProfileButton";
+import { faHome, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { Button } from "@/components/ui/button";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export default function NavBar() {
   const { user } = useContext(UserContext)
   const { push } = useRouter();
   const pathname = usePathname()
-  
-  if( pathname === '/login') return <></>
+
+  const options = [
+    { title: "", icon: faHome, action: () => push("/") },
+    { title: "Doações entregues", icon: null, action: () => push("/doacoes/entregues") },
+    { title: "Acompanhar suas doações", icon: null, action: () => push("/doacoes/acompanhar") },
+    { title: "Virar entregador", icon: null, action: () => push("/virar-entregador") }
+  ]
+
+  if (pathname === '/login') return <></>
 
   return (
-    <div className="flex justify-center mt-4">
-      <nav className="flex justify-center bg-green-700 h-10 p-4 items-center text-white rounded-2xl border-gradient">
-        <ul className="flex h-full items-center">
-          <li
-            onClick={() => push("/")}
-            className="h-full items-center flex self-center rounded-2xl p-4 cursor-pointer hover:bg-green-600 hover:font-semibold  active:bg-green-700 duration-100"
-          >
-            <FontAwesomeIcon
-              icon={faHome}
-              size="lg"
-            />
-          </li>
-          <li
-            className="h-full items-center flex self-center rounded-2xl p-4 cursor-pointer hover:bg-green-600 hover:font-semibold  active:bg-green-700 duration-100"
-            onClick={() => push("/doacoes/entregues")}
-          >
-            Doações entregues
-          </li>
-          <li
-            className="h-full items-center flex p-4 rounded-2xl cursor-pointer hover:bg-green-600 hover:font-semibold  active:bg-green-700 duration-100"
-            onClick={() => push("/doacoes/acompanhar")}
-          >
-            Acompanhar suas doações
-          </li>
-          {!user?.transporter_id && (
-            <li
-              className="h-full items-center flex p-4 rounded-2xl cursor-pointer hover:bg-green-600 hover:font-semibold"
-              onClick={() => push("/virar-entregador")}
+    <div className="flex justify-center mt-3">
+      <nav className="flex justify-center bg-primary px-2 py-1 items-cente text-secondary rounded-xl custom-shadow" >
+        <div className="grid grid-flow-col auto-cols-max gap-2.5">
+          {options.map((option, index) => (
+            <Button
+              key={index}
+              className="flex items-center p-2 hover:bg-green-600 hover:font-semibold active:bg-green-700 duration-100 rounded-lg"
+              size="sm"
+              onClick={option.action}
             >
-              Virar entregador
-            </li>
-          )}
-        </ul>
-      </nav>
-      <ProfileButton></ProfileButton>
-    </div>
+              {option.icon && <FontAwesomeIcon icon={faHome} size="lg" />}
+              {option.title}
+            </Button>
+          ))}
+          <div className="border-l text-secondary h-9 mx-1" />
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Avatar className="w-9 h-9 cursor-pointer" onClick={() => console.log("teste")}>
+                <AvatarImage sizes="sm" src="https://github.com/shadcn.png" alt="@shadcn" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-40">
+              <DropdownMenuLabel>Minha conta</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <FontAwesomeIcon icon={faRightFromBracket} className="mr-2 h-4 w-4" />
+                <span>Log out</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </nav >
+    </div >
   );
 }
--- src/components/ProfileButton.tsx
+++
@@ -1,7 +0,0 @@
-export default function ProfileButton() {
-  return (
-    <div className="absolute top-4 right-4 flex w-10 h-10 rounded-full bg-green-700 border-gradient text-white items-center justify-center">
-      <p>AB</p>
-    </div>
-  );
-}
