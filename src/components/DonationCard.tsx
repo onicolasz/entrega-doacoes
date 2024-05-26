e72553273620b9016cfc5ecb537df5cb2678b8ce
Leonardo Mayca <leomt8440@gmail.com>
2024-05-26 19:15:44 -0300

    feat: new style from donation card.

--- src/components/DonationCard.tsx
+++ src/components/DonationCard.tsx
@@ -3,6 +3,14 @@ import { useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import { faCarSide } from "@fortawesome/free-solid-svg-icons";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 interface IUser {
   name?: string;
@@ -51,24 +59,40 @@ const DonationCard: React.FC<CardProps> = ({
         return "Entregue";
       case "INCOMING":
         return "A caminho";
+      default:
+        return status;
     }
   };
 
   return (
-    <div className="flex p-4 bg-white text-black border border-green-700 text-xs rounded-2xl items-center m-4 drop-shadow-lg">
-      <div className="flex-1">
+    <div className="grid grid-cols-[55%,2%,30%,2%,11%] p-4 bg-white border border-primary text-xs rounded-2xl items-center m-4 drop-shadow-lg">
+      <div className="col-span-1 h-full flex flex-col justify-between">
         <div className="flex items-center mb-2">
-          <div className="rounded-2xl p-2 bg-gray-200 font-medium flex items-center justify-center text-xs mr-2">
+          <div className="rounded-xl px-2 py-1 bg-secondary font-medium flex items-center justify-center text-xs mr-2">
             {userName}
           </div>
           <div>
-            {firstItem.quantity}{" "}
-            {firstItem.quantity > 1 ? "unidades" : "unidade"} de{" "}
+            {firstItem.quantity}
+            {firstItem.quantity > 1 ? " unidades " : " unidade "}de{" "}
             {firstItem.name} e
             {additionalItems > 0 && (
-              <span className="ml-1 bg-slate-200 rounded-full p-1">
-                +{additionalItems} {additionalItems > 1 ? "itens" : "item"}
-              </span>
+              <TooltipProvider>
+                <Tooltip delayDuration={0}>
+                  <TooltipTrigger asChild>
+                    <span className="cursor-default ml-1 bg-secondary rounded-full px-2 py-1">
+                      +{additionalItems} {additionalItems > 1 ? "itens" : "item"}
+                    </span>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    {items.map((item, index) => {
+                      if (index == 0) return <></>
+
+                      return (<p>{item.quantity} {item.quantity > 1 ? "unidades" : "unidade"} de {item.name}</p>)
+                    })}
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+
             )}
           </div>
         </div>
@@ -81,49 +105,47 @@ const DonationCard: React.FC<CardProps> = ({
           </div>
         </div>
       </div>
-      <div className="flex-2 ml-4">
-        <p className="text-lg font-semibold">Total de itens {totalQuantity}</p>
-        <p>Peso total {totalQuantity}kg</p>
-        <p>Distância 2km</p>
+      <Separator orientation="vertical" className="h-full bg-primary" />
+      <div className="col-span-1 h-full flex flex-col justify-center">
+        <div>
+          <p className="text-lg"><span className="font-semibold">Total de itens:</span> {totalQuantity}</p>
+          <p>Peso total: {totalWeight}kg</p>
+        </div>
+        {!!user?.transporter_id && (
+          <div>
+            <Button className="flex items-center p-1 h-8 mt-1 hover:bg-green-600 hover:font-semibold active:bg-green-700 duration-100 rounded-lg">
+              Aceitar entrega
+            </Button>
+          </div>
+        )}
       </div>
-      {!!user?.transporter_id && (
-        <div className="flex-2 ml-4">
-          <button className="bg-green-700 text-white rounded-xl p-2 border border-slate-200 hover:bg-green-500 active:bg-green-700 duration-100">
-            Aceitar entrega
-          </button>
-        </div>
-      )}
-      {!!user?.transporter_id && (
-        <div className="flex-2 ml-4">
-          <button className="bg-green-700 text-white rounded-xl p-2 border border-slate-200 hover:bg-green-500 active:bg-green-700 duration-100">
-            Aceitar entrega
-          </button>
-        </div>
-      )}
-      {deliveryStatus === "FINISHED" && (
-        <div className="flex flex-col items-center ml-4">
-          <FontAwesomeIcon icon={faCircleCheck} size="3x" color="green" />
-          {transporter && (
-            <div className="font-medium mt-2 text-center">
-              <p>
-                {parseStatus(deliveryStatus)} por {transporter.name}
-              </p>
-            </div>
-          )}
-        </div>
-      )}
-      {deliveryStatus === "INCOMING" && (
-        <div className="flex flex-col items-center ml-4">
-          <FontAwesomeIcon icon={faCarSide} size="3x" color="orange" />
-          {transporter && (
-            <div className="font-medium mt-2 text-center">
-              <p>
-                {parseStatus(deliveryStatus)} por {transporter.name}
-              </p>
-            </div>
-          )}
-        </div>
-      )}
+      <Separator orientation="vertical" className="h-full bg-primary" />
+      <div className="col-span-1 h-full flex flex-col items-center justify-center">
+        {deliveryStatus === "FINISHED" && (
+          <>
+            <FontAwesomeIcon icon={faCircleCheck} size="3x" color="green" />
+            {transporter && (
+              <div className="font-medium mt-2 text-center">
+                <p>
+                  {parseStatus(deliveryStatus)} por {transporter.name}
+                </p>
+              </div>
+            )}
+          </>
+        )}
+        {deliveryStatus === "INCOMING" && (
+          <>
+            <FontAwesomeIcon icon={faCarSide} size="3x" color="orange" />
+            {transporter && (
+              <div className="font-medium mt-2 text-center">
+                <p>
+                  {parseStatus(deliveryStatus)} por {transporter.name}
+                </p>
+              </div>
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 };
