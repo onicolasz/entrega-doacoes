78640c47fb5ffc8529482b1dae3eb140f41bf078
bernardosevero <bernardoseverosilveira@gmail.com>
2024-05-25 21:53:40 -0300

    ✨ feat: redirect to login when not logged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 'use client'
 
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const { data: session } = useSession();
+  const {push} = useRouter()
+  
   if (!session) {
-    console.log("não ta logado");
+    push('/login')
   }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Olá Mundo</h1>
