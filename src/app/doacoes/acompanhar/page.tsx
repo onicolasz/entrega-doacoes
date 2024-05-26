a9649eddfc0ad3fd154c84df3815798a84eb0365
onicolasz <nicolasbarrosdesouza@gmail.com>
2024-05-26 17:54:30 -0300

    feat: doacoes entregues page and acompanhar page

--- src/app/doacoes/acompanhar/page.tsx
+++ src/app/doacoes/acompanhar/page.tsx
@@ -1,10 +1,67 @@
 "use client";
 
-export default function AcompanharDoacoes() {
+import DonationCard from "@/components/DonationCard";
 
+const data = [
+  {
+    userName: "Nicolas",
+    items: [
+      { name: "Feijão", quantity: 10, weigth: 100 },
+      { name: "Arroz", quantity: 5, weigth: 100 },
+      { name: "Item 3", quantity: 5, weigth: 100 },
+      { name: "Item 4", quantity: 5, weigth: 100 },
+    ],
+    from_location: "Rua general lima e silva 250 porto alegre",
+    destination_location: "RUA JUSTINO CAMOBIM 994 SAPUCAIA DO SUL",
+    delivery: {
+      status: 'FINISHED',
+      transporter: {
+        user: {
+          name: 'Zé da Pinga'
+        }
+      }
+    },
+  },
+  {
+    userName: "Giuseppe Cadura",
+    items: [
+      { name: "Feijão", quantity: 10, weigth: 100 },
+      { name: "Arroz", quantity: 5, weigth: 100 },
+      { name: "Item 3", quantity: 5, weigth: 100 },
+      { name: "Item 4", quantity: 5, weigth: 100 },
+    ],
+    from_location: "Rua general lima e silva 250 porto alegre",
+    destination_location: "RUA JUSTINO CAMOBIM 994 SAPUCAIA DO SUL",
+    delivery: {
+      status: 'INCOMING',
+      transporter: {
+        user: {
+          name: 'Zé da Manga'
+        }
+      }
+    },
+  },
+];
+
+export default function DoacoesEntregues() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Acompanhar doações</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-14">
+      <div className="min-h-screen w-[700px]">
+        <p className="text-2xl text-black font-semibold py-4 pl-4">
+          Acompanhar doações
+        </p>
+        {data.map((item, index) => (
+          <DonationCard
+            key={index}
+            userName={item.userName}
+            items={item.items}
+            fromLocation={item.from_location}
+            destinationLocation={item.destination_location}
+            deliveryStatus={item.delivery.status}
+            transporter={item?.delivery?.transporter?.user}
+          />
+        ))}
+      </div>
     </main>
   );
 }
--- src/components/DonationCard.tsx
+++ src/components/DonationCard.tsx
@@ -1,5 +1,12 @@
 import { UserContext } from "@/app/contexts/user.context";
 import { useContext } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
+import { faCarSide } from "@fortawesome/free-solid-svg-icons";
+
+interface IUser {
+  name?: string;
+}
 
 type Item = {
   name: string;
@@ -12,6 +19,8 @@ type CardProps = {
   items: Item[];
   fromLocation: string;
   destinationLocation: string;
+  deliveryStatus: string;
+  transporter?: IUser;
 };
 
 const DonationCard: React.FC<CardProps> = ({
@@ -19,20 +28,34 @@ const DonationCard: React.FC<CardProps> = ({
   items,
   fromLocation,
   destinationLocation,
+  deliveryStatus,
+  transporter,
 }) => {
   const { user } = useContext(UserContext);
   const firstItem = items[0];
   const additionalItems = items.length - 1;
 
-  const { totalQuantity, totalWeight } = items.reduce((acc, currentItem) => {
-    return {
-      totalQuantity: acc.totalQuantity + currentItem.quantity,
-      totalWeight: acc.totalWeight + currentItem.weigth,
-    };
-  }, { totalQuantity: 0, totalWeight: 0 });
+  const { totalQuantity, totalWeight } = items.reduce(
+    (acc, currentItem) => {
+      return {
+        totalQuantity: acc.totalQuantity + currentItem.quantity,
+        totalWeight: acc.totalWeight + currentItem.weigth,
+      };
+    },
+    { totalQuantity: 0, totalWeight: 0 }
+  );
+
+  const parseStatus = (status: string) => {
+    switch (status) {
+      case "FINISHED":
+        return "Entregue";
+      case "INCOMING":
+        return "A caminho";
+    }
+  };
 
   return (
-    <div className="flex p-4 mb-4 bg-white text-black text-sm rounded-2xl items-center m-4">
+    <div className="flex p-4 bg-white text-black border border-green-700 text-xs rounded-2xl items-center m-4 drop-shadow-lg">
       <div className="flex-1">
         <div className="flex items-center mb-2">
           <div className="rounded-2xl p-2 bg-gray-200 font-medium flex items-center justify-center text-xs mr-2">
@@ -65,7 +88,40 @@ const DonationCard: React.FC<CardProps> = ({
       </div>
       {!!user?.transporter_id && (
         <div className="flex-2 ml-4">
-          <button className="bg-green-700 text-white rounded-xl p-2 border border-slate-200 hover:bg-green-500 active:bg-green-700 duration-100">Aceitar entrega</button>
+          <button className="bg-green-700 text-white rounded-xl p-2 border border-slate-200 hover:bg-green-500 active:bg-green-700 duration-100">
+            Aceitar entrega
+          </button>
+        </div>
+      )}
+      {!!user?.transporter_id && (
+        <div className="flex-2 ml-4">
+          <button className="bg-green-700 text-white rounded-xl p-2 border border-slate-200 hover:bg-green-500 active:bg-green-700 duration-100">
+            Aceitar entrega
+          </button>
+        </div>
+      )}
+      {deliveryStatus === "FINISHED" && (
+        <div className="flex flex-col items-center ml-4">
+          <FontAwesomeIcon icon={faCircleCheck} size="3x" color="green" />
+          {transporter && (
+            <div className="font-medium mt-2 text-center">
+              <p>
+                {parseStatus(deliveryStatus)} por {transporter.name}
+              </p>
+            </div>
+          )}
+        </div>
+      )}
+      {deliveryStatus === "INCOMING" && (
+        <div className="flex flex-col items-center ml-4">
+          <FontAwesomeIcon icon={faCarSide} size="3x" color="orange" />
+          {transporter && (
+            <div className="font-medium mt-2 text-center">
+              <p>
+                {parseStatus(deliveryStatus)} por {transporter.name}
+              </p>
+            </div>
+          )}
         </div>
       )}
     </div>
